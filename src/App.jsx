aebbbe72ed47b0d2387751e3004e1191ae348027
Ansh Jagwal <anshjagwal02@gmail.com>
2025-12-25 23:26:33 +0530

    feat: improve carousel navigation logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,27 @@ import CATS from "./data/cats";
 import { FaArrowCircleLeft } from "react-icons/fa";
 import { FaArrowCircleRight } from "react-icons/fa";
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   const [catImage, setImage] = useState(0);
+
   return (
-    <div className="flex justify-center items-center  ">
-      <div className="">
-        {catImage > 0 && (
-          <button
-            onClick={() => {
-              if (catImage > 0) {
-                setImage(catImage - 1);
-              }
-            }}
-            className="text-5xl "
-          >
-            <FaArrowCircleLeft />
-          </button>
-        )}
-      </div>
-      <div className="">
-        <Newcomp cat={CATS[catImage]} />
-      </div>
-      <div className="">
-        {catImage < 4 ? (
-          <button
-            onClick={() => {
-              if (catImage < 4) {
-                setImage(catImage + 1);
-              }
-            }}
-            className="text-5xl"
-          >
-            <FaArrowCircleRight />
-          </button>
-        ): null}
-      </div>
+    <div className="flex justify-center items-center">
+      <Analytics />
+
+      {catImage > 0 && (
+        <button onClick={() => setImage(catImage - 1)} className="text-5xl">
+          <FaArrowCircleLeft />
+        </button>
+      )}
+
+      <Newcomp cat={CATS[catImage]} />
+
+      {catImage < CATS.length - 1 && (
+        <button onClick={() => setImage(catImage + 1)} className="text-5xl">
+          <FaArrowCircleRight />
+        </button>
+      )}
     </div>
   );
 };
